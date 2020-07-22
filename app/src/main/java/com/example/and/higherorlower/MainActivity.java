9a5f1ef6aa14b24770b29dfd2cd81e8f806fd066
Andressa <andressacsferreira@gmail.com>
2020-07-23 00:24:59 +0100

    renova o numero depois de acertar

--- app/src/main/java/com/example/and/higherorlower/MainActivity.java
+++ app/src/main/java/com/example/and/higherorlower/MainActivity.java
@@ -9,34 +9,45 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String randStr;
+    int rand_int1;
     Random rand = new Random();
-    int rand_int1 = rand.nextInt(20)+1;
-    String randStr= String.valueOf(rand_int1);
+    String message;
 
+    public void newRandomNumber(){
+        rand_int1 = rand.nextInt(20)+1;
+        randStr= String.valueOf(rand_int1);
+    }
 
     public void guessFunction(View view){
         Log.i("My number", randStr);
+
         EditText guessedN = (EditText) findViewById(R.id.guessedNumber);
         if (! guessedN.getText().toString().matches("")) {
             int guessedNint = Integer.parseInt(guessedN.getText().toString());
             if (guessedNint == rand_int1) {
-                Toast.makeText(this, "Acertô miserávi! O número é " + rand_int1 + ".", Toast.LENGTH_LONG).show();
+                message="Acertô miserávi! O número é " + rand_int1 + ". Tente de novo.";
+                newRandomNumber();
+                guessedN.setText("");
+
             } else if (guessedNint > 20) {
-                Toast.makeText(this, "O número deve ser menor que 20.", Toast.LENGTH_LONG).show();
+                message="O número deve ser menor que 20.";
                 guessedN.setText("");
             } else if (guessedNint < 1) {
-                Toast.makeText(this, "O número deve ser maior que 0.", Toast.LENGTH_LONG).show();
+                message="O número deve ser maior que 0.";
                 guessedN.setText("");
             } else if (guessedNint < rand_int1) {
-                Toast.makeText(this, "Tente um número maior.", Toast.LENGTH_LONG).show();
+                message="Tente um número maior.";
                 guessedN.setText("");
             } else if (guessedNint > rand_int1) {
-                Toast.makeText(this, "Tente um número menor.", Toast.LENGTH_LONG).show();
+                message="Tente um número menor.";
                 guessedN.setText("");
             }
         } else {
-            Toast.makeText(this, "Digite um número entre 1 e 20.", Toast.LENGTH_LONG).show();
+            message="Digite um número entre 1 e 20.";
         }
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
 
 
     }
@@ -45,5 +56,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newRandomNumber();
     }
 }
